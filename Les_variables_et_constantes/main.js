0ab68089d167138ace8a0528fd09ecf4e52a0e67
WILDWWM <wpetit@cosformation.fr>
2022-10-24 11:12:29 +0200

    Résultats

---
+++ Les_variables_et_constantes/main.js
@@ -0,0 +1,19 @@
+let nb1 = 2;
+let nb2 = 5;
+let chaine1 = "Bonjour ";
+let chaine2 = "le monde !";
+let varBool1 = true;
+let varBool2 = false;
+
+const varFixe = 3;
+
+console.log(nb1 + nb2);
+console.log(chaine1 + chaine2);
+console.log(varBool1 + varBool2);
+
+console.log(nb1 + chaine1);
+console.log(nb1 + varBool1);
+console.log(chaine1 + varBool1);
+
+varFixe = 5;
+// Le changement de valeur d'une contante provoque une erreur à l'exécution.
