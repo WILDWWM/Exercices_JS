1ca2fb46571ec433d210dee9eff8ac06ee2f2e83
WILDWWM <wpetit@cosformation.fr>
2022-10-25 11:05:56 +0200

    Modif

--- Les_fonctions/Exo1/main.js
+++ Les_fonctions/Exo1/main.js
@@ -1,31 +1,32 @@
 function monJourSemaine() {
     let monJour = new Date();
     let reponse = monJour.getDay();
+    switch (reponse) {
+        case 1:
+            console.log("Lundi");
+            break;
+        case 2:
+            console.log("Mardi");
+            break;
+        case 3:
+            console.log("Mercredi");
+            break;
+        case 4:
+            console.log("Jeudi");
+            break;
+        case 5:
+            console.log("Vendredi");
+            break;
+        case 6:
+            console.log("Samedi");
+            break;
+        case 7:
+            console.log("Dimanche");
+            break;
+    }
     return reponse;
 }
 
-key = monJourSemaine();
+console.log(monJourSemaine());
 
-switch (key) {
-    case 1:
-        console.log("Lundi");
-        break;
-    case 2:
-        console.log("Mardi");
-        break;
-    case 3:
-        console.log("Mercredi");
-        break;
-    case 4:
-        console.log("Jeudi");
-        break;
-    case 5:
-        console.log("Vendredi");
-        break;
-    case 6:
-        console.log("Samedi");
-        break;
-    case 7:
-        console.log("Dimanche");
-        break;
-}
+
