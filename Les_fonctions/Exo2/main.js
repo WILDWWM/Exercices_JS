ab743a060005b3c454c37f0b6fc476152288e000
WILDWWM <wpetit@cosformation.fr>
2022-10-25 08:53:03 +0200

    Résultat presque parfait

---
+++ Les_fonctions/Exo2/main.js
@@ -0,0 +1,44 @@
+const groupe = [42, 67, 46, 39, 72, 55, 39, 64, 44];
+let valeurObjet = {};
+
+function ageTool(monTab) {
+    let age = { moyenne: 0, lePlusJeune: 0, lePlusAge: 0};
+
+    // Calcul de la moyenne
+    let somme = 0;
+    for (let i = 0; i < monTab.length; i++) {
+        somme = somme + monTab[i];        
+    }
+    age.moyenne = somme / monTab.length;
+
+    // Calcul du plus jeune
+    let valeurPlusPetite;
+    for (let j = 0; j < monTab.length; j++) {
+        if (monTab[j] < monTab[j+1]) {
+            valeurPlusPetite = monTab[j];            
+        }
+        else {
+            valeurPlusPetite = monTab[j + 1];
+        }
+    }
+    age.lePlusJeune = valeurPlusPetite;
+
+    // Calcul du plus âgé
+    let valeurPlusGrande;
+    for (let k = 0; k < monTab.length; k++) {
+         if (monTab[k] > monTab[k+1]) {
+             valeurPlusGrande = monTab[k];
+        }       
+         else {
+             valeurPlusGrande = monTab[k + 1];
+        }
+    }
+    age.lePlusAge = valeurPlusGrande;
+
+    return age;
+}
+
+valeurObjet = ageTool(groupe);
+console.log(valeurObjet.moyenne);
+console.log(valeurObjet.lePlusJeune);
+console.log(valeurObjet.lePlusAge);
