ea0961216518c51854c10c10431e93ee2774b2e6
WILDWWM <wpetit@cosformation.fr>
2022-10-25 09:31:44 +0200

    Exo 1

--- Les_boucles/Exo1/main.js
+++ Les_boucles/Exo1/main.js
@@ -1,2 +1,4 @@
 const groupe = [42, 67, 46, 39, 72, 55, 39, 64, 44];
 
+console.log("La personne la plus jeune a " + groupe.sort()[0] + " ans.");
+console.log("La personne la plus âgée a " + groupe.sort()[groupe.length-1] + " ans.");
